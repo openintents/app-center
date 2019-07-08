60284fc666944671c0a75d3ec33e86b23b6ecbf9
Friedger Müffke <mail@friedger.de>
2019-07-08 11:37:58 +0200

    remove public folder

--- public/styles-70f5345d2f58bc44dbf2.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{412:function(n,w,o){}}]);
-//# sourceMappingURL=styles-70f5345d2f58bc44dbf2.js.map
--- public/styles-7bad090194efa4df995e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{352:function(n,w,o){}}]);
-//# sourceMappingURL=styles-7bad090194efa4df995e.js.map
